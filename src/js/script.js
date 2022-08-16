2fceaa89a4326fecdf4eef7b1c66edaad0f69144
outofs <rtranscendental@gmail.com>
2022-08-16 23:55:31 +0300

    adding cart modal

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+const navList = document.querySelector(".nav__list");
+const menuBtn = document.querySelector(".menu-icon");
+const productImg = document.querySelector(".product-img");
+const smallImg = document.querySelectorAll(".small-img");
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+const indicator = document.querySelector(".indicator");
+const loginBtn = document.querySelector(".loginBtn");
+const registerBtn = document.querySelector(".registerBtn");
+
+///Toggle Menu
+menuBtn.addEventListener("click", function (e) {
+  navList.classList.toggle("hidden");
+});
+
+///Choosing image
+smallImg.forEach((img) =>
+  img.addEventListener("click", function (e) {
+    productImg.src = img.src;
+  })
+);
+
+///Toggle Form
+
+registerBtn.addEventListener("click", function (e) {
+  registerForm.style.transform = "translateX(0px)";
+  loginForm.style.transform = "translateX(0px)";
+  indicator.style.transform = "translateX(100px)";
+});
+
+loginBtn.addEventListener("click", function (e) {
+  registerForm.style.transform = "translateX(300px)";
+  loginForm.style.transform = "translateX(300px)";
+  indicator.style.transform = "translateX(0px)";
+});
