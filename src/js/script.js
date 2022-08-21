3fabfd49158e537384faacb7fc7474c885539746
outofs <rtranscendental@gmail.com>
2022-08-22 01:16:42 +0300

    final commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,6 @@ const navList = document.querySelector(".nav__list");
 const menuBtn = document.querySelector(".menu-icon");
 const productImg = document.querySelector(".product-img");
 const smallImg = document.querySelectorAll(".small-img");
-const loginForm = document.querySelector(".login-form");
-const registerForm = document.querySelector(".register-form");
-const indicator = document.querySelector(".indicator");
-const loginBtn = document.querySelector(".loginBtn");
-const registerBtn = document.querySelector(".registerBtn");
 
 ///Toggle Menu
 menuBtn.addEventListener("click", function (e) {
@@ -19,20 +14,6 @@ smallImg.forEach((img) =>
     productImg.src = img.src;
   })
 );
-
-///Toggle Form
-
-// registerBtn.addEventListener("click", function (e) {
-//   registerForm.style.transform = "translateX(0px)";
-//   loginForm.style.transform = "translateX(0px)";
-//   indicator.style.transform = "translateX(100px)";
-// });
-
-// loginBtn.addEventListener("click", function (e) {
-//   registerForm.style.transform = "translateX(300px)";
-//   loginForm.style.transform = "translateX(300px)";
-//   indicator.style.transform = "translateX(0px)";
-// });
 
 ///Cart
 const cartIcon = document.querySelector(".cart-icon");
